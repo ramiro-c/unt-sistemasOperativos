4d66cd607c32723a3e81901422e0a38e49b7de1a
ramiro-c <50562689+ramiro-c@users.noreply.github.com>
2019-11-24 03:48:23 -0300

    script para ejecutar programas en C que usan hilos

--- run-c-thread.sh
+++ run-c-thread.sh
@@ -3,6 +3,6 @@
 if [ $1 ];then
 	gcc "$1".c -o "$1" -pthread
 	./"$1"
-else
-	echo -e "\nDebe enviar el nombre del programa sin la extension como parametro.\n"
+	exit
 fi
+echo -e "\nDebe enviar el nombre del programa sin la extension como parametro.\n"
